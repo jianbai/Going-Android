edc088e472bb525f4e8b7c4304fcacd4491ea91a
jianbai <scott_wang@brown.edu>
2014-12-30 22:00:48 -0500

    incremented seek bars in settings

--- app/src/main/java/io/spw/hello/SettingsActivity.java
+++ app/src/main/java/io/spw/hello/SettingsActivity.java
@@ -2,6 +2,7 @@ package io.spw.hello;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.parse.ParseUser;
@@ -18,6 +19,10 @@ public class SettingsActivity extends ActionBarActivity {
     protected TextView settingsName;
     protected TextView settingsAge;
     protected TextView settingsHometown;
+    protected TextView settingsAgeSpread;
+    protected SeekBar settingsAgeSeekBar;
+    protected TextView settingsGenderSpread;
+    protected SeekBar settingsGenderSeekBar;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,5 +48,73 @@ public class SettingsActivity extends ActionBarActivity {
             e.printStackTrace();
         }
         settingsHometown.setText(hometown);
+
+        // Set up seek bars
+        settingsAgeSeekBar = (SeekBar) findViewById(R.id.settings_age_seek_bar);
+        settingsAgeSpread = (TextView) findViewById(R.id.settings_age_spread_indicator);
+        settingsGenderSeekBar = (SeekBar) findViewById(R.id.settings_gender_seek_bar);
+        settingsGenderSpread = (TextView) findViewById(R.id.settings_gender_spread_indicator);
+
+        settingsAgeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 0:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_default);
+                        break;
+                    case 1:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_1);
+                        break;
+                    case 2:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_2);
+                        break;
+                    case 3:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_3);
+                        break;
+                    case 4:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_4);
+                        break;
+                    default:
+                        settingsAgeSpread.setText(R.string.settings_age_spread_default);
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        settingsGenderSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 0:
+                        settingsGenderSpread.setText(R.string.settings_gender_spread_default);
+                        break;
+                    case 1:
+                        settingsGenderSpread.setText(R.string.settings_gender_spread_1);
+                        break;
+                    case 2:
+                        settingsGenderSpread.setText(R.string.settings_gender_spread_2);
+                        break;
+                    default:
+                        settingsGenderSpread.setText(R.string.settings_gender_spread_default);
+                        break;
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
     }
 }
